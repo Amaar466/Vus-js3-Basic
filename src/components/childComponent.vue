f4a4e3b0ba4456dd2e52baf5c23d3ceb445d9968
sajidthedev <a.dev.inside@gmail.com>
2024-05-21 10:10:25 +0500

    parent to child pass data

---
+++ src/components/childComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>{{name}}</h2>
+    <h2>{{user.email}}</h2>
+    <button v-on:click="getData()">Click</button>
+</template>
+
+<script>
+export default {
+    name:"childComponent",
+    props:{
+        name:String,
+        user:Object,
+        getData:Function
+    }
+}
+</script>
---
+++ src/components/parentCompenent.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>Pass Data Parent to Child</h1>
+<childComponent name="Amaar ul haq" :user="user"  :getData="getData"/>
+</template>
+
+<script>
+import childComponent from './childComponent.vue';
+export default {
+    name:'parentComponent',
+    components:{
+        childComponent
+    },
+    data() {
+        return {
+            user:{name:'bilal', email:'bilal@gmail.com'},
+        }
+    },
+    methods: {
+        getData(){
+            console.warn('Parent Function Called');
+        }
+    },
+}
+</script>
